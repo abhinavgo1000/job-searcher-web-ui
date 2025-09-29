4cca1df4bdc960fe19d6a5cfe437837f441e7740
Abhinav Goel <abhinavgo1000@gmail.com>
2025-09-29 17:41:52 +0530

    unused import removed

--- context/theme-toggle-provider.tsx
+++ context/theme-toggle-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 
 import theme from '@/theme';
 
